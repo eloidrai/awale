2a40426c750ae50951227437a3a31b8aac9a85ce
Éloi Draï <eloidrai@gmail.com>
2019-08-13 23:08:05 +0200

    Semailles opérationelles ainsi que les prises

--- awale.py
+++ awale.py
@@ -13,8 +13,12 @@ class Partie:
         r = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "f":6, "e":7, "d":8, "c":9, "b":10, "a":11}[lettre]      # On associe à la lettre son rang
         graines = self.liste[r]     # On récupère le nombre de graines dans le trou
         self.liste[r] = 0       # On vide la case que l'on choisit
-        for i in range(r+1, r+graines+1):
-            self.liste[i%12] += 1   # On sème
+        i = r
+        while graines>0:
+            i+=1
+            if i%12!=r: # Si on n'est pas au départ
+                self.liste[i%12] += 1   # On sème
+                graines-=1
         prises = []                 # Liste des prises (avant de savoir si on affame)
         while (self.liste[i%12]==2 or self.liste[i%12]==3) and ((self.joueur1 and i%12>5) or (not self.joueur1 and i%12<6)):        # On calcule les prises
             prises.append(i%12)

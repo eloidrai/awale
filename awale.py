5bb6b1c80f57864de12e693ec0cde5a5b73b972f
Éloi Draï <eloidrai@gmail.com>
2019-08-14 16:32:10 +0200

    Ajout du test de fin et amélioration de tour

--- awale.py
+++ awale.py
@@ -4,28 +4,41 @@ from termcolor import *
 
 print(" "*15+colored("Jeu d'awalé", "red", "on_yellow", ["bold"]))
 
-class Partie:
+class Partie(object):
     def __init__(self):
-        self.liste = [4,4,4,1,1,1,1,4,4,4,4,4]
+        self.liste = [4,4,0,0,0,0,0,0,0,0,0,0]
         self.joueur1 = True
         self.graines_joueur1 = 0
         self.graines_joueur2 = 0
     
-    def coup(self, lettre):
-        r = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "f":6, "e":7, "d":8, "c":9, "b":10, "a":11}[lettre]      # On associe à la lettre son rang
-        graines = self.liste[r]     # On récupère le nombre de graines dans le trou
-        self.liste[r] = 0       # On vide la case que l'on choisit
-        i = r
+    def fin(self):
+        if self.joueur1 and sum(self.liste[6:])==0:
+            for i in range(0,6):
+                if self.liste[i]>5-i:
+                    return False
+            return True
+        elif not self.joueur1 and sum(self.liste[:12])==0:
+            for i in range(6,12):
+                if self.liste[i]>11-i:
+                    return False
+            return True
+        else:
+            return False
+    
+    def coup(self, trou_depart):
+        graines = self.liste[trou_depart]     # On récupère le nombre de graines dans le trou
+        self.liste[trou_depart] = 0       # On vide la case que l'on choisit
+        trou = trou_depart
         while graines>0:
-            i+=1
-            if i%12!=r: # Si on n'est pas au départ
-                self.liste[i%12] += 1   # On sème
+            trou+=1
+            if trou%12!=trou_depart: # Si on n'est pas au départ
+                self.liste[trou%12] += 1   # On sème
                 graines-=1
         prises = []                 # Liste des prises (avant de savoir si on affame)
-        while (self.liste[i%12]==2 or self.liste[i%12]==3) and ((self.joueur1 and i%12>5) or (not self.joueur1 and i%12<6)):        # On calcule les prises
-            prises.append(i%12)
-            i-=1
-        if ((self.joueur1 and len([i for i in self.liste[6:] if i==0])+len(prises)==6) or (not self.joueur1 and len([i for i in self.liste[:6] if i==0])+len(prises)==6)) or len(prises)==0: #On ne prend rien
+        while (self.liste[trou%12]==2 or self.liste[trou%12]==3) and ((self.joueur1 and trou%12>5) or (not self.joueur1 and trou%12<6)):        # On calcule les prises
+            prises.append(trou%12)
+            trou-=1
+        if ((self.joueur1 and len([i for i in self.liste[6:] if i==0])+len(prises)==6) or (not self.joueur1 and len([i for i in self.liste[:6] if i==0])+len(prises)==6)): # On ne prend rien (famine)
             pass
         else:                       # Prise(s) normale(s)
             for i in prises:
@@ -34,7 +47,11 @@ class Partie:
                 self.graines_joueur1 += len(prises)
             else:
                 self.graines_joueur2 += len(prises)
-        
-#    def tour(self):
-#        self.coup(input("Quelle case jouez-vous ?"))
-#        self.joueur1 = !self.joueur1
+    
+    def tour(self):
+        t = int(input("Choisir un numéro de case : "))
+        while not ((self.joueur1 and 0<=t<6) or (not self.joueur1 and 5<t<=11)) or self.liste[t]==0:
+            t = int(input("Choisir un numéro de case %s : " %(colored("valide", on_color='on_red'))))
+        self.coup(t)
+        print(self.liste)
+        self.joueur1 = not self.joueur1

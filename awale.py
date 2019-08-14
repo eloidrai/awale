cb3687077722996471dc619758e8f8102f09d9f2
Éloi Draï <eloidrai@gmail.com>
2019-08-14 22:43:33 +0200

    Correction de l'apport final

--- awale.py
+++ awale.py
@@ -2,11 +2,13 @@
 
 class Partie(object):
     def __init__(self):
-        self.liste = [4,4,0,0,0,0,0,0,0,0,0,0]
+        self.liste = [4, 1, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 1]
         self.joueur1 = True
+        self.fin = False
         self.graines_joueur1 = 0
         self.graines_joueur2 = 0
     
+    @property
     def jouables(self):
         j = tuple(i for i in list(range(0,6) if self.joueur1 else range(6,12)) if self.liste[i]!=0)
         if self.joueur1 and sum(self.liste[6:])==0:
@@ -29,15 +31,34 @@ class Partie(object):
         while (self.liste[trou%12]==2 or self.liste[trou%12]==3) and ((self.joueur1 and trou%12>5) or (not self.joueur1 and trou%12<6)):
             prises.append(trou%12)
             trou-=1
-        if ((self.joueur1 and len([i for i in self.liste[6:] if i==0])+len(prises)==6) or (not self.joueur1 and len([i for i in self.liste[:6] if i==0])+len(prises)==6)): # On ne prend rien (famine)
+        self.gain = len(prises)
+        if ((self.joueur1 and len([i for i in self.liste[6:] if i==0])+self.gain==6) or (not self.joueur1 and len([i for i in self.liste[:6] if i==0])+self.gain==6)):
             pass
         else:
             for i in prises:
                 self.liste[i] = 0
             if self.joueur1:
-                self.graines_joueur1 += len(prises)
+                self.graines_joueur1 += self.gain
             else:
-                self.graines_joueur2 += len(prises)
+                self.graines_joueur2 += self.gain
     
     def tour(self):
-        pass
+        print("--Joueur n°1 (%s pts)--" %self.graines_joueur1 if self.joueur1 else "--Joueur n°2 (%s pts)--" %self.graines_joueur2)
+        t = int(input("Choisissez un nombre %s : " %str(self.jouables)))
+        self.coup(t)
+        print(self.liste)
+        if self.gain!=0:
+            print("+1 point" if self.gain==1 else "+ %s points" %str(self.gain))
+        self.joueur1 = not self.joueur1
+        if self.jouables==():
+            if self.joueur1:
+                self.graines_joueur1+=sum(self.liste)
+            else:
+                self.graines_joueur2+=sum(self.liste)
+            self.fin = True
+            print("Partie terminée.")
+            print("Le joueur n° %s a gagné." %("1" if self.graines_joueur1>self.graines_joueur2 else "2"))
+
+p = Partie()
+while not p.fin:
+    p.tour()
